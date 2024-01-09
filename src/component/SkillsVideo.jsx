858916c0eaba82623d93cd4733da623dfd6de8a2
Heidi Leivo <heidi@geegokids.com>
2024-01-09 09:29:37 +0000

    Update SkillsVideo.jsx

--- src/component/SkillsVideo.jsx
+++ src/component/SkillsVideo.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import "./Video.css";
 
-
 const SkillsVideo = (props) => {
     return (props.trigger) ?
     (<div className="Video" style={{ display: props.display }}>
         <div className="dummy-block"></div>
-      <video controls>
-        <source src={props.video} type="video/mp4" />
-      </video>
-      <div>
-        <button 
-            className="closeButton" 
-            onClick={() => props.setTrigger(false)}>SULJE
-        </button>
-        <h4>{props.title}</h4>
-        <p>{props.description}</p>
+        <video controls loop> {/* Added loop attribute here */}
+          <source src={props.video} type="video/mp4" />
+        </video>
+        <div>
+          <button 
+              className="closeButton" 
+              onClick={() => props.setTrigger(false)}>SULJE
+          </button>
+          <h4>{props.title}</h4>
+          <p>{props.description}</p>
+        </div>
       </div>
-    </div>) : ("");
+    ) : ("");
 };
 
-export default SkillsVideo
+export default SkillsVideo;
