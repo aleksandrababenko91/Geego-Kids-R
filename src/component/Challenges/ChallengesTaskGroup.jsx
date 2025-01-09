043f602254730eb72c1a8d145b6a5bb2e99e05eb
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2025-01-09 10:06:51 +0200

    fixed styles Challenges

--- src/component/Challenges/ChallengesTaskGroup.jsx
+++ src/component/Challenges/ChallengesTaskGroup.jsx
@@ -5,18 +5,12 @@ import "./ChallengesTaskGroup.css";
 
 const ChallengesTaskGroup = (props) => {
   return (
-    <div>
-      <Link to={`/Challenges/${props.title}`}>
-        <div className="task-group-cardContainer">
-          <div className="task-group-card">
-            <img src={props.imgUrl} alt={props.title}   className="iconWrapper"/>
-          </div>
-          <div className="task-group-header">
-            <h2>{props.title}</h2>
-          </div>
-        </div>
-      </Link>
-    </div>
+    <Link to={`/Challenges/${props.title}`}>
+      <div className="task-group-cardContainer">
+        <img src={props.imgUrl} alt={props.title} fill sizes="100%"  className="iconWrapper"/>
+        <h2 className="task-group-header">{props.title}</h2>
+      </div>
+    </Link>
   );
 };
 
