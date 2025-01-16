12da9ad4c96aa6efa18ba89357cc4fcafdb34c60
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2025-01-16 14:13:35 +0200

    burger styles done

--- src/component/Header/BurgerMenu/BurgerMenu.jsx
+++ src/component/Header/BurgerMenu/BurgerMenu.jsx
@@ -1,9 +1,6 @@
-
-
 import './BurgerMenu.css'
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-
 
 export default function BurgerMenu() {
   const [burgerMenuActive, setBurgerMenuActive] = useState(false);
@@ -50,38 +47,35 @@ export default function BurgerMenu() {
 
   return (
     <div className={`navbar ${burgerMenuActive ? 'active' : ''}`}>
-      <div className="navigation">
+      <div className="navigationMain">
         <div
           className="burgerMenuContainer"
           onClick={() => toggleBurgerMenu()}
         >
           <div className="burgerMenuTrigger"></div>
-
           <div className="burgerMenu"></div>
-
         </div>
       </div>
-      <div className="content">
-
+      <div className="contentBurger">
         <motion.ul
           animate={burgerMenuActive ? 'open' : 'closed'}
           variants={motionVariants}
         >
-           <motion.li variants={listItemVariants}>
-                <a href="/geegocity">Geegocity</a>
-              </motion.li>
-              <motion.li variants={listItemVariants}>
-                <a href="/sporttiparkkki">Sporttiparkki</a>
-              </motion.li>
-              <motion.li variants={listItemVariants}>
-                <a href="/">Koulu</a>
-              </motion.li>
-              <motion.li variants={listItemVariants}>
-                <a href="/">Tietosuoja</a>
-              </motion.li>
-              <motion.li variants={listItemVariants}>
-                <a href="/">Ladata tiedosta</a>
-              </motion.li>
+          <motion.li variants={listItemVariants}>
+            <a href="/Challenges">Geegocity</a>
+          </motion.li>
+          <motion.li variants={listItemVariants}>
+            <a href="/Skills">Sporttiparkki</a>
+          </motion.li>
+          <motion.li variants={listItemVariants}>
+            <a href="/Toddlers">Koulu</a>
+          </motion.li>
+          <motion.li variants={listItemVariants}>
+            <a href="/">Tietosuoja</a>
+          </motion.li>
+          <motion.li variants={listItemVariants}>
+            <a href="/">Ladata tiedosta</a>
+          </motion.li>
         </motion.ul>
       </div>
     </div>
