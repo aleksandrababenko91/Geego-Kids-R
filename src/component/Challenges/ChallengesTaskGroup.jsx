c91ea64b945d30dd4f180bf1a1a4895b5b034e95
aleksandrababenko91 <127403692+aleksandrababenko91@users.noreply.github.com>
2025-01-27 13:15:55 +0200

    added animation DINO AOs

--- src/component/Challenges/ChallengesTaskGroup.jsx
+++ src/component/Challenges/ChallengesTaskGroup.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./ChallengesTaskGroup.css";
+import MainLink from '../shared/MainLink/MainLink';
 
 
 const ChallengesTaskGroup = (props) => {
   return (
-      <Link to={`/Challenges/${props.title}`} >
+      <MainLink href={`/Challenges/${props.title}`} >
         <div className="task-group-cardContainer">
           <div className="task-group-card">
             <img src={props.imgUrl} alt="pic of task"  className="iconWrapper"/>
@@ -14,7 +15,7 @@ const ChallengesTaskGroup = (props) => {
             <h2 className="task-group-header">{props.title}</h2>
           </div>
         </div>
-      </Link>
+      </MainLink>
   );
 };
 
